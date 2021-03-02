d4f2ca25571dd1171fec313155b6aa0fe58f049c
Kyle Chine <kyle.chine@leismore.co>
2021-03-02 14:16:09 +1100

    Export public types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
  * A Node.js & Express.js handler function for rejecting non-allowed-HTTP-methods.
  */
 
-import * as express from 'express';
-import { LMError }  from '@leismore/lmerror';
+import express = require('express');
+import { LMError, Res }  from '@leismore/lmerror';
 
-function generator(allowed: string[], error: LMError):
-  (req:express.Request, _res:express.Response, next:express.NextFunction) => void
+type ExpressRoutingHandler = (req:express.Request, _res:express.Response, next:express.NextFunction) => void;
+
+function generator(allowed: string[], error: LMError): ExpressRoutingHandler
 {
   for (const k in allowed)
   {
@@ -30,12 +31,6 @@ function generator(allowed: string[], error: LMError):
   return all_handler;
 }
 
-// Res type for describing HTTP response info.
-type Res = {                                            // HTTP response
-  readonly statusCode:  string,                         // HTTP response status code
-           headers?:   {readonly [key:string]: string}, // HTTP headers
-           body?:       any                             // HTTP body
-};
 // Generate Res object for LMError response parameter
 function gen_response(allowed: string[]):Res
 {
@@ -51,4 +46,4 @@ function gen_response(allowed: string[]):Res
   return response;
 }
 
-export { generator, gen_response };
+export { generator, gen_response, ExpressRoutingHandler, Res as LMErrorRes };
